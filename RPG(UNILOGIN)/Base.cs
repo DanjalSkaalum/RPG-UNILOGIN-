1e02b02b4bf2215d6d4d4f7712d7a25e90606829
Dánjal Skaalum <danjal.skaalum@gmail.com>
2023-12-15 09:07:18 +0100

    Add project files.

---
+++ RPG(UNILOGIN)/Base.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_UNILOGIN_
+{
+    internal abstract class Base
+    {
+        string _Name;
+        int _HP;
+        int _XP;
+
+        string Name { get { return _Name; } set { _Name = value; } }
+        int HP { get { return _HP; } set { _HP = value; } }
+        int XP { get { return _XP; } set { _XP = value; } }
+
+        public void Attack()
+        {
+
+        }
+        public void Damage()
+        {
+
+        }
+        public void IsDead()
+        {
+
+        }
+    }
+}
---
+++ RPG(UNILOGIN)/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RPG_UNILOGIN_
+{
+    internal class Player : Base
+    {
+        string _Name;
+        int _HP;
+        int _XP;
+        string Name { get { return _Name; } set { _Name = value; } }
+        int HP { get { return _HP; } set { _HP = value; } }
+        int XP { get { return _XP; } set { _XP = value; } }
+
+        public void Attack()
+        {
+
+        }
+    }
+}
